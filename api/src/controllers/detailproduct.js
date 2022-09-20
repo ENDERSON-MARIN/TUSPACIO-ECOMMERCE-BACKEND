35af8ae603126aedec98e597427f80c4777229f0
Edinson <edinsonmadrid@gmail.com>
2022-09-19 22:59:48 -0300

    Update detailproduct.js

--- api/src/controllers/detailproduct.js
+++ api/src/controllers/detailproduct.js
@@ -8,27 +8,27 @@ const getDetailProduct = async (req, res, next) => {
     const id = req.params.id;
 
     try {
-            // const dbInfo = await Product.findOne({
-            //     where: { id },
-            //     include: {
-            //         model: Categorie,
-            //         attributes: ["name"],
-            //         through: { attributes: [] },
-            //     },
-            // });
-            // const results = await Review.findAll({
-            //     where: { product_id: id },
+            const dbInfo = await Product.findOne({
+                where: { id },
+                include: {
+                    model: Categorie,
+                    attributes: ["name"],
+                    through: { attributes: [] },
+                },
+            });
+            const results = await Review.findAll({
+                where: { product_id: id },
             
-            // });
-            // const reviews = results.map(e => ({
-            //     title: e.title,
-            //     text: e.text,
-            //     score: e.score,
-            //     user_id: e.user_id,
+            });
+            const reviews = results.map(e => ({
+                title: e.title,
+                text: e.text,
+                score: e.score,
+                user_id: e.user_id,
                 
-            // }))
-            res.send("entre detail")
-            // res.send({dbInfo, reviews});
+            }))
+            // res.send("entre detail")
+            res.send({dbInfo, reviews});
            } catch (error) {
              console.log(error);
          }
