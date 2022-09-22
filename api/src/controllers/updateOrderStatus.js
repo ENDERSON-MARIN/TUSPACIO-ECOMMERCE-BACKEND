a953629167d81bea4b009c2310baac72a326c504
Enderson Marín <marinenderson1@gmail.com>
2022-09-22 16:24:34 -0500

    SEND EMAIL ORDER COMPLETED!

--- api/src/controllers/updateOrderStatus.js
+++ api/src/controllers/updateOrderStatus.js
@@ -1,22 +1,28 @@
 const { Order } = require("../db");
+const sendEmailOrders = require("../helpers/sendEmailOrders");
 
-const updateOrderStatus = async ( req, res ) => {
+const updateOrderStatus = async (req, res) => {
   const { id } = req.params;
   const { status } = req.body;
-    try {
-      if(!status){
-          res.status(400).send("Status is required");
-      } else{
-        const dbInfo = await Order.update(
-          { status },
-          { where: { number: id } }
-        );
-        console.log("Order Status Successfully Updated!")
-        res.status(200).send(dbInfo);
+  try {
+    if (!status) {
+      res.status(400).send("Status is required");
+    } else {
+      const dbInfo = await Order.update({ status }, { where: { number: id } });
+      if (dbInfo.status === "completed") {
+        const user = {
+          name: dbInfo.shipping.name,
+          email: dbInfo.shipping.email,
+        };
+        sendEmailOrders.sendMail(user);
+        console.log("send email Order Completed!");
       }
-    } catch (error) {
-      console.log(error);
+      console.log("Order Status Successfully Updated!");
+      res.status(200).send(dbInfo);
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
